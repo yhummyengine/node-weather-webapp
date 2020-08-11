4ae45f09398f25c413d10885c29caa53bced88c9
yomi oyekanmi <yomioye27@gmail.com>
2020-08-11 11:22:57 -0700

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -6,34 +6,27 @@ console.log("client side javascript file is loaded!");
 //   });
 // });
 
+const weatherform = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
+weatherform.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const location = search.value;
 
-const weatherform = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
 
-
-
-weatherform.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    
-    const location = search.value
-
-    messageOne.textContent = 'loading...'
-    messageTwo.textContent = ''
-    
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error 
-            } else {
-                messageOne.textContent = data.location
-                messageOne.textContent = data.forecast
-            }
-        })
-    })
-    
-})
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageOne.textContent = data.forecast;
+      }
+    });
+  });
+});
